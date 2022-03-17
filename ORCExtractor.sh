bf25d1138dbf95ec77703291f93f469158f55b54
Dupss <sebastiendups@gmail.com>
2022-03-17 12:47:50 +0000

    Update script

--- ORCExtractor.sh
+++ ORCExtractor.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+usage() {                                 # Function: Print a help message.
+  echo "Usage: $0 [ -i ORCs DIRECTORY ] [ -p PASSWORD ]" 1>&2
+  exit 1
+}
+
 #
-while getopts r:p: flag
+while getopts i:p:h: flag
 do
     case "${flag}" in
-        r) dir=${OPTARG};;
+        i) collect_dir=${OPTARG};;
         p) password=${OPTARG};;
-    esac
+        *) usage;;
+  esac
 done
-echo $password
-echo $dir
+
 
 # Decompression des fichiers .7z de facon recursive
-nb=$(find . -name "*.7z" |wc -l)
+nb=$(find $collect_dir -name "*.7z" |wc -l)
 if [ "$nb" -ne 0 ];
 then
-	for fic in $(find . -name "*.7z")
+	for fic in $(find $collect_dir -name "*.7z")
 	do
 	    echo "Decompressing $fic ... "
 	    dir="${fic%%.7z}"
@@ -29,7 +34,7 @@ then
 	done
 
 echo "Removing \"data\" extension"
-for file in $(find . -name "*_data")
+for file in $(find $collect_dir -name "*_data")
     do
 	    newname=$(echo ${file} | sed 's/\_data//g')
 	    mv $file $newname
